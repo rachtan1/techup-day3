af11f57629e93c793efae1f2ef203735fbd0bee9
rachtan1 <rachtan@gmail.com>
2025-05-14 07:52:09 +0000

    fix bug

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 
 // Needed for Express
 var express = require('express')
+var axios = require('axios');
 var app = express()
 
 // Needed for EJS
@@ -97,23 +98,18 @@ app.post("/delete/:id", async (req, res) => {
   });
 
 // Tells the app which port to run on
-app.get('/demo',function(req,res){res.render('pages/demo');})
+app.get('/demo',function(req,res){res.render('pages/demo');});
+// give me weather
+app.get('/weather', async (req, res) => {
+  try {
+    const response = await axios.get('https://api-open.data.gov.sg/v2/real-time/api/twenty-four-hr-forecast');
+    res.render('pages/weather', { weather: response.data });
+  } catch (error) {
+    console.error(error);
+    res.send('Error fetching weather data');
+  }
+  });
+
 app.listen(8080);
 
-// add this snippet after "var express = require('express')"
-var axios = require('axios');
-...
 
-
-// add this snippet before 
-app.get('/weather', async (req, res) => {
-    try {
-      const response = await axios.get('https://api-open.data.gov.sg/v2/real-time/api/twenty-four-hr-forecast');
-      res.render('pages/weather', { weather: response.data });
-    } catch (error) {
-      console.error(error);
-      res.send('Error fetching weather data');
-    }
-  });
-  }
-});
